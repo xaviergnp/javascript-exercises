b3fdb613fb2ecac1a2750b8f9b35b72f14526a9e
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-13 14:43:48 +0800

    Add rounding and temperature formula

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+function roundTwoDecimal(num) {
+  return Math.round( ( num + Number.EPSILON ) * 100 ) / 100;
+}
+const convertToCelsius = function(temp) {
+  return roundTwoDecimal((temp-32) * (5/9))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return roundTwoDecimal(temp * (9/5) + 32)
 };
 
 // Do not edit below this line
