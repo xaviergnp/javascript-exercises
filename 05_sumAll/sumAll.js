efd42bf6f6d251e33d8f6faf8130867094a9e99f
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-13 14:16:30 +0800

    Use isInteger and Add error return

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(numStart,numEnd) {
+    if (Number.isInteger(numStart) && Number.isInteger(numEnd) && numStart >=0 && numEnd >=0) {
+        if (numStart>numEnd) {
+            let tmp = numStart;
+            numStart=numEnd;
+            numEnd=tmp;
+        }
+        let sum=0;
+        for(let x =numStart; x<=numEnd; x++) sum+=x;
+        return sum;
+    } else {
+        return "ERROR"
+    }
+    
 };
 
 // Do not edit below this line
