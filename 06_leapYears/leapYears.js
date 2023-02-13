f8851ed389b536bd80a43ddad6e695e19ac299db
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-13 14:27:41 +0800

    Add divisible by 100 check on logic

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,5 @@
 const leapYears = function(num) {
-    if (num%4==0 || (num%100==0 && num%400==0)) return true;
+    if ((num%4==0 && num%100!=0) || (num%100==0 && num%400==0)) return true;
     else return false;
 };
 
