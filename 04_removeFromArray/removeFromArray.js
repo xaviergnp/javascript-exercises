58f1d6825ce54ec1a7ca4d10c52dc2390df9773d
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-13 15:03:59 +0800

    Change arguments to rest operator

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,10 @@
-const removeFromArray = function(arr, delNum) {
-    let filteredArray = arr;
-    for (args of Array.from(arguments)) {
-        filteredArray = filteredArray.filter((num) => num !== args);
-    }
-    return filteredArray;
+const removeFromArray = function(arr, ...delNum) {
+    // let filteredArray = arr;
+    // for (args of Array.from(arguments)) {
+    //     filteredArray = filteredArray.filter((num) => num !== args);
+    // }
+    // return filteredArray;
+    return arr.filter((num) => !delNum.includes(num) );
 };
 
 // Do not edit below this line
