591689608dfad67b6ea2938ce9be584d8e5696e5
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-13 11:42:12 +0800

    Pass test cases

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let rev = "";
+    for(let x= String(str).length-1; x>=0; x--){
+        rev+=String(str).charAt(x);
+    }
+    return rev;
 };
 
 // Do not edit below this line
