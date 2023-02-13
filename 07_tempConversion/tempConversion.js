8c27ce90587da19e4ff663f9884d75260455cae0
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-13 14:45:26 +0800

    Change rounding to one decimal

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,12 @@
-function roundTwoDecimal(num) {
-  return Math.round( ( num + Number.EPSILON ) * 100 ) / 100;
+function roundOneDecimal(num) {
+  return Math.round( ( num + Number.EPSILON ) * 10 ) / 10;
 }
 const convertToCelsius = function(temp) {
-  return roundTwoDecimal((temp-32) * (5/9))
+  return roundOneDecimal((temp-32) * (5/9))
 };
 
 const convertToFahrenheit = function(temp) {
-  return roundTwoDecimal(temp * (9/5) + 32)
+  return roundOneDecimal(temp * (9/5) + 32)
 };
 
 // Do not edit below this line
