199d34f607cf3f253f95884ad09bdb54d1358200
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-13 14:22:41 +0800

    Add logic to function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function(num) {
+    if (num%4==0 || (num%100==0 && num%400==0)) return true;
+    else return false;
 };
 
 // Do not edit below this line
