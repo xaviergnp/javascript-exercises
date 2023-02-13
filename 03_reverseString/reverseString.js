553545e749d4239be6eedf734cab1cb16966c7f0
xaviergnp <120711776+xaviergnp@users.noreply.github.com>
2023-02-13 11:46:54 +0800

    Use reverse method

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,9 +1,10 @@
 const reverseString = function(str) {
-    let rev = "";
-    for(let x= String(str).length-1; x>=0; x--){
-        rev+=String(str).charAt(x);
-    }
-    return rev;
+    // let rev = "";
+    // for(let x= String(str).length-1; x>=0; x--){
+    //     rev+=String(str).charAt(x);
+    // }
+    // return rev;
+    return String(str).split("").reverse().join("");
 };
 
 // Do not edit below this line
